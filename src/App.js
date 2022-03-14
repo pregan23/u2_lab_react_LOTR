aa73347860777fd756e6eca2fe4f12bdc36380a1
pregan23 <pregan2bing@gmail.com>
2022-03-14 16:21:58 -0400

    array of objects

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@ import React from 'react'
 import './App.css'
 import Movie from './components/Movie.js'
 
+
+
+
 const App = () => {
+
+  let movies = [
+    {
+      'title': 'The Fellowship of the Ring',
+      'hours': '2',
+      'minutes': '58',
+      'poster': 'https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg',
+    },
+    {
+      'title': 'The Two Towers',
+      'hours': '2',
+      'minutes': '59',
+      'poster': 'https://image.tmdb.org/t/p/original/rrGlNlzFTrXFNGXsD7NNlxq4BPb.jpg',
+    },
+    {
+      'title': 'The Return of the King',
+      'hours': '3',
+      'minutes': '21',
+      'poster': 'https://image.tmdb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg'
+    }
+  
+  
+  ]
 
   return (
     <div className="App">
@@ -10,12 +36,17 @@ const App = () => {
         <h1>Lord of the Rings: A Trilogy</h1>
       </header>
       <main>
-        <Movie title = 'The Fellowship of the Ring' hours = '2' minutes = '58' poster = 'https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg' year = '2001' />
-        <Movie title = 'The Two Towers' hours = '2' minutes = '59' poster = 'https://image.tmdb.org/t/p/original/rrGlNlzFTrXFNGXsD7NNlxq4BPb.jpg' year = '2002' />
-        <Movie title = 'The Return of the King' hours = '3' minutes = '21' poster = 'https://image.tmdb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg' year = '2003' />
+      
+        <Movie movie = {movies[0]} />
+
+
       </main>
     </div>
   )
 }
 
 export default App
+
+{/* <Movie title = 'The Fellowship of the Ring' hours = '2' minutes = '58' poster = 'https://image.tmdb.org/t/p/original/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg' year = '2001' />
+        <Movie title = 'The Two Towers' hours = '2' minutes = '59' poster = 'https://image.tmdb.org/t/p/original/rrGlNlzFTrXFNGXsD7NNlxq4BPb.jpg' year = '2002' />
+        <Movie title = 'The Return of the King' hours = '3' minutes = '21' poster = 'https://image.tmdb.org/t/p/original/rCzpDGLbOoPwLjy3OAm5NUPOTrC.jpg' year = '2003' /> */}
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,18 @@
 import React from 'react';
+// import movies from './App'
 
-const Movie = (movie) => {
+const Movie = (props) => {
+
+    
     // const movieTitle = 
     return (
         <div>
-            <img src = {movie.poster}/>
-            <h1>Lord of the Rings: {movie.title} </h1>
-            <p>{movie.year} Runtime: {movie.hours} hrs, {movie.minutes} minutes </p>
+            <img src = {props.movie.poster}/>
+            <h1>Lord of the Rings: {props.movie.title} </h1>
+            <p>{props.movie.year} - Runtime: {props.movie.hours} hrs, {props.movie.minutes} minutes - Rated PG-13</p>
             
         </div>
     ) 
+    
 };
 export default Movie;
